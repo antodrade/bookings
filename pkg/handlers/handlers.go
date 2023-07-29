8ba96259d0d65749213fd87223ee4712b5915cc9
prueba2@gmail.com <prueba@gmail.com>
2023-07-28 19:04:07 -0500

    finish 8.03 Creating a handler that return JSON

--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -1,10 +1,12 @@
 package handlers
 
 import (
+	"encoding/json"
+	"fmt"
 	"net/http"
-
-	"github.com/antodrade/bookings/pkg/models"
+	"log"
 	"github.com/antodrade/bookings/pkg/config"
+	"github.com/antodrade/bookings/pkg/models"
 	"github.com/antodrade/bookings/pkg/render"
 )
 
@@ -29,9 +31,9 @@ func NewHandlers(r *Repository) {
 
 // Home is the home page handler
 func (m *Repository) Home(w http.ResponseWriter, r *http.Request) {
-	  remoteIP := r.RemoteAddr
-	  m.App.Session.Put(r.Context(),"remote_ip", remoteIP)
-	render.RenderTemplate(w, "home.page.html", &models.TemplateData{})
+	remoteIP := r.RemoteAddr
+	m.App.Session.Put(r.Context(), "remote_ip", remoteIP)
+	render.RenderTemplate(w, "home.page.html", r, &models.TemplateData{})
 }
 
 // About is the about page Handler
@@ -42,51 +44,68 @@ func (m *Repository) About(w http.ResponseWriter, r *http.Request) {
 
 	remoteIP := m.App.Session.GetString(r.Context(), "remote_ip")
 
-   
-
 	stringMap["remote_ip"] = remoteIP
 
-	
-
 	// send the data to the template
-	render.RenderTemplate(w, "about.page.html", &models.TemplateData{
+	render.RenderTemplate(w, "about.page.html", r, &models.TemplateData{
 		StringMap: stringMap,
 	})
 
-
-}
-  
-//Reservation renders the make a reservation page and dislplays form
-func (m* Repository) Reservation(w http.ResponseWriter, r *http.Request){
-	render.RenderTemplate(w, "make-reservation.page.html", &models.TemplateData{})
 }
 
-//Generals renders the room page
-func (m* Repository) Generals(w http.ResponseWriter, r *http.Request){
-	render.RenderTemplate(w, "generals.page.html", &models.TemplateData{})
+// Reservation renders the make a reservation page and dislplays form
+func (m *Repository) Reservation(w http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(w, "make-reservation.page.html", r, &models.TemplateData{})
 }
 
-//Majors renders the room page
-func (m* Repository) Majors(w http.ResponseWriter, r *http.Request){
-	render.RenderTemplate(w, "majors.page.html", &models.TemplateData{})
+// Generals renders the room page
+func (m *Repository) Generals(w http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(w, "generals.page.html", r, &models.TemplateData{})
+}
+
+// Majors renders the room page
+func (m *Repository) Majors(w http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(w, "majors.page.html", r, &models.TemplateData{})
 }
 
 // Availability renders the search availability page
-func (m* Repository) Availability(w http.ResponseWriter, r *http.Request){
-	render.RenderTemplate(w, "search-availability.page.html", &models.TemplateData{})
+func (m *Repository) Availability(w http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(w, "search-availability.page.html", r, &models.TemplateData{})
 }
 
 // PostAvailability renders the search availability page
-func (m* Repository) PostAvailability(w http.ResponseWriter, r *http.Request){
-	w.Write([]byte("Posted to search availability"))
+func (m *Repository) PostAvailability(w http.ResponseWriter, r *http.Request) {
+	start:= r.Form.Get("start")
+	end:= r.Form.Get("end")
+
+	w.Write([]byte(fmt.Sprintf("start date is %s and end date is %s", start, end)))
 }
 
-
-//COntact render the contact page
-func (m* Repository) Contact(w http.ResponseWriter, r *http.Request){
-	render.RenderTemplate(w, "contact.page.html", &models.TemplateData{})
+type jsonResponse struct {
+	OK      bool `json:"ok"`
+	Message string `json: "message"`
 }
 
-func (m* Repository) MakeReservation(w http.ResponseWriter, r *http.Request){
-	render.RenderTemplate(w, "make-reservation.page.html", &models.TemplateData{})
+func (m *Repository) AvailabilityJSON(w http.ResponseWriter, r *http.Request) {
+	resp:= jsonResponse{
+		OK: false,
+		Message: "Available!",
+	}
+	out, err := json.MarshalIndent(resp, "","      ")
+	if err != nil{
+		log.Println(err)
+	}
+    
+	
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(out)
+}
+
+// COntact render the contact page
+func (m *Repository) Contact(w http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(w, "contact.page.html", r, &models.TemplateData{})
+}
+
+func (m *Repository) MakeReservation(w http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(w, "make-reservation.page.html", r,  &models.TemplateData{})
 }
--- pkg/render/render.go
+++ pkg/render/render.go
@@ -10,6 +10,7 @@ import (
 
 	"github.com/antodrade/bookings/pkg/config"
 	"github.com/antodrade/bookings/pkg/models"
+	"github.com/justinas/nosurf"
 )
 
 var functions = template.FuncMap{}
@@ -21,12 +22,13 @@ func NewTemplate(a *config.AppConfig) {
 	app = a
 }
 
-func AddDefaultData(td *models.TemplateData) *models.TemplateData {
+func AddDefaultData(td *models.TemplateData, r *http.Request) *models.TemplateData {
+	td.CSFRToken = nosurf.Token(r)
 	return td
 }
 
 // RenderTemplate renders templates using html/template
-func RenderTemplate(w http.ResponseWriter, tmpl string, td *models.TemplateData) {
+func RenderTemplate(w http.ResponseWriter, tmpl string, r *http.Request, td *models.TemplateData)  {
 	//get the template cache from the app config
 
 	var tc map[string]*template.Template
@@ -43,7 +45,7 @@ func RenderTemplate(w http.ResponseWriter, tmpl string, td *models.TemplateData)
 
 	buf := new(bytes.Buffer)
 
-	td = AddDefaultData(td)
+	td = AddDefaultData(td, r)
 
 	_ = t.Execute(buf, td)
 
