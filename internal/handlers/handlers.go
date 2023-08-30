7b51be7a649108fcea77cf5f6cd4ccd1d5a2a192
prueba2@gmail.com <prueba@gmail.com>
2023-08-29 20:32:09 -0500

    min 7:55 8.08 Server side form validation II

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -64,7 +64,18 @@ func (m *Repository) Reservation(w http.ResponseWriter, r *http.Request) {
 
 // PostReservation handles the post of a reservation form
 func (m *Repository) PostReservation(w http.ResponseWriter, r *http.Request) {
-	
+	err := r.ParseForm()
+	if err != nil{
+		log.Println(err)
+		return
+	}
+
+	reservation:= models.Reservation{
+		FirstName: r.Form.Get("first_name"),
+		LastName:  r.Form.Get("last_name"),
+		Phone:     r.Form.Get("phone"),
+		Email:     r.Form.Get("email"),
+	}
 }
 
 // Generals renders the room page
